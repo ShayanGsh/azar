ffe98603a4b07de7b32d155fdf3b6450151fe94a
Klaus <klaus_hybrid33@yahoo.com>
2023-01-27 01:16:38 +0330

    disabled ssl

--- api/config.go
+++ api/config.go
@@ -34,13 +34,13 @@ type Database struct {
 
 func (db *Database) ToConnString() (string) {
     var buf bytes.Buffer
-	fmt.Fprintf(&buf, "host=%s port=%d user=%s password=%s dbname=%s", db.Host, db.Port, db.Username, db.Password, db.Name)
+	fmt.Fprintf(&buf, "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", db.Host, db.Port, db.Username, db.Password, db.Name, "disable")
     return buf.String()
 }
 
 func (c *Config) ToConnString() (string) {
 	var buf bytes.Buffer
-	fmt.Fprintf(&buf, "host=%s port=%d user=%s password=%s dbname=%s", c.DatabaseHost, c.DatabasePort, c.DatabaseUsername, c.DatabasePassword, c.DatabaseName)
+	fmt.Fprintf(&buf, "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", c.DatabaseHost, c.DatabasePort, c.DatabaseUsername, c.DatabasePassword, c.DatabaseName, "disable")
     return buf.String()
 }
 
