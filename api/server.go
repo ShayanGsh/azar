989dc40070d036e81b0abb40e4f99defa443dbaa
Klaus <klaus_hybrid33@yahoo.com>
2023-04-23 07:07:39 +0330

    added docs

--- api/server.go
+++ api/server.go
@@ -14,6 +14,8 @@ import (
 	"github.com/pseidemann/finish"
 )
 
+// Server is the main struct for the application. It holds the router, config, and other
+// important parts of the application.
 type Server struct {
 	Router *chi.Mux
 	Config *Config
@@ -28,6 +30,10 @@ type Server struct {
 }
 
 
+// NewServer creates a new server instance
+// using the provided configuration.
+// It initializes a DB pool and
+// a new JWTAuth instance.
 func NewServer(c *Config) *Server {
 	s := &Server {
 		Router: chi.NewRouter(),
@@ -42,41 +48,49 @@ func NewServer(c *Config) *Server {
 	return s
 }
 
+// SetupRoutes sets up the routes and middleware for the server.
+// The routes are grouped by authentication level. The JWTAuth
+// middleware is used to verify the token in the header, and
+// the AuthenticateAccess middleware is used to verify that the
+// user has the correct access level to access the route.
 func (s *Server) setupRoutes() {
-
 	SetupMiddlewares(s.Router)
-
 	s.Router.Group(func(r chi.Router) {
 		AuthenticateAccess(r.(*chi.Mux), s.JWTAuth.JWTAuth)
 	})
-
 	s.Router.Post("/login", s.UserControllers.Login)
 	s.Router.Post("/register", s.UserControllers.Register)
 }
 
+// createDBPool creates a connection pool to the database.
 func (s *Server) createDBPool() {
 	connConfig, err := pgxpool.ParseConfig(s.Config.ToConnString())
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	s.DB, err = pgxpool.NewWithConfig(context.Background(), connConfig)
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
-func (s *Server) MigrationCheck() bool{
+func (s *Server) MigrationCheck() bool {
+	// Open a connection to the database
 	conn, err := sql.Open("postgres", s.Config.ToConnString())
 	if err != nil {
 		panic(err)
 	}
+
+	// Check if the database is migrated
 	status, err := db.IsMigrated(db.Migration("./"), conn)
 	if err != nil {
 		panic(err)
 	}
+
+	// If the database is not migrated, run the migration
 	if !status {
 		err = db.RunMigration(db.Migration("./"), conn, 0)
 		if err != nil {
@@ -89,6 +103,11 @@ func (s *Server) MigrationCheck() bool{
 	}
 }
 
+// This function starts a server. It creates a new HTTP server and sets
+// the address to the address of the server. It creates a finisher and
+// adds the HTTP server to it. It starts listening on the HTTP server
+// and sets the finisher to the server. It waits for the finisher to
+// finish.
 func (s *Server) Start() {
 	httpServer := &http.Server{
 		Addr: s.Config.Address(),
@@ -99,6 +118,15 @@ func (s *Server) Start() {
 	fin := finish.New()
 	fin.Add(httpServer)
 
+	s.startListening(httpServer)
+	s.Finish = fin
+	s.started = true
+	fin.Wait()
+}
+
+// startListening starts a goroutine to listen for incoming HTTP requests.
+// It will also shut down the server if there is an error.
+func (s *Server) startListening(httpServer *http.Server) {
 	go func() {
 		err := httpServer.ListenAndServe()
 		if err != nil {
@@ -107,21 +135,26 @@ func (s *Server) Start() {
 			log.Println(err)
 		}
 	}()
-	s.Finish = fin
-	s.started = true
-	fin.Wait()
 }
 
+// IsRunning returns true if the server is running and false if it is not.
 func (s *Server) IsRunning() bool {
 	return s.started
 }
 
+
+// Shutdown closes the database connection and
+// triggers the Finish channel.
 func (s *Server) Shutdown() {
 	s.DB.Close()
 	s.Finish.Trigger()
 	s.started = false
 }
 
+// Wait blocks until the server is done.
+//
+// The server is done when there are no more
+// connections.
 func (s *Server) Wait() {
 	s.Finish.Wait()
 }
