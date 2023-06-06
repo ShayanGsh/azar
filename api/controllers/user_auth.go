9dacc034af0a4bef959754f74daf3d2bdcfab703
Klaus <klaus_hybrid33@yahoo.com>
2023-06-06 23:02:36 +0330

    moved struct

--- api/controllers/user_auth.go
+++ api/controllers/user_auth.go
@@ -17,14 +17,6 @@ type User struct {
     Password string `json:"password" validate:"required,min=8"`
 }
 
-type UpdateUser struct {
-	Username    string `json:"username" validate:"required_without=Email,omitempty,min=1,max=100,excludesall=0x20"`
-	Email   string `json:"email" validate:"required_without=Username,omitempty,email"`
-	NewEmail    string `json:"new_email" validate:"omitempty,email"`
-	OldPassword string `json:"old_password" validate:"required,min=8"`
-	NewPassword string `json:"new_password" validate:"min=8"`
-}
-
 type UserController struct {
 	Controller
 }
--- api/controllers/user_operations.go
+++ api/controllers/user_operations.go
@@ -1,1 +1,9 @@
 package controllers
+
+type UpdateUser struct {
+	Username    string `json:"username" validate:"required_without=Email,omitempty,min=1,max=100,excludesall=0x20"`
+	Email   string `json:"email" validate:"required_without=Username,omitempty,email"`
+	NewEmail    string `json:"new_email" validate:"omitempty,email"`
+	OldPassword string `json:"old_password" validate:"required,min=8"`
+	NewPassword string `json:"new_password" validate:"min=8"`
+}
