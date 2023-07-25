0eb270131651f9b3821def2e11683f4830f1f50b
Shayan <shayan1997g@gmail.com>
2023-07-25 16:33:52 +0330

    refactor: added an interface for single policies

--- core/perms/policy.go
+++ core/perms/policy.go
@@ -2,11 +2,25 @@ package perms
 
 import "github.com/ShayanGsh/azar/core/errors"
 
-type Policy interface{
+type Policies interface{
 	IsAllowed(action string, resource string) bool
 	AddPolicy(name string, description string, action string, resource string) error
+	AddPolicyByObject(policy Policy) error
 	RemovePolicy(name string) error
 	GetPolicy(name string) (PolicyData, error)
+	IterPolicies() <- chan PolicyData
+}
+
+type Policy interface {
+	IsAllowed(action string, resource string) bool
+	GetPolicyName() string
+	GetPolicyDescription() string
+	GetPolicyAction() string
+	GetPolicyResource() string
+	SetPolicyName(name string)
+	SetPolicyDescription(description string)
+	SetPolicyAction(action string)
+	SetPolicyResource(resource string)
 }
 
 type PolicyData struct{
@@ -49,6 +63,26 @@ func (pl *PolicyList) AddPolicy(name string, description string, action string, 
 	return nil
 }
 
+func (pl *PolicyList) AddPolicyByObject(policy Policy) error{
+	if _, ok := pl.Policies[policy.GetPolicyName()]; ok {
+		return errors.ErrPolicyNameExists
+	}
+
+	for _, p := range pl.Policies {
+		if p.Action == policy.GetPolicyAction() && p.Resource == policy.GetPolicyResource() {
+			return errors.ErrPolicyExists
+		}
+	}
+
+	pl.Policies[policy.GetPolicyName()] = PolicyData{
+		Name: policy.GetPolicyName(),
+		Description: policy.GetPolicyDescription(),
+		Action: policy.GetPolicyAction(),
+		Resource: policy.GetPolicyResource(),
+	}
+	return nil
+}
+
 func (pl *PolicyList) RemovePolicy(name string) error{
 	if _, ok := pl.Policies[name]; !ok {
 		return errors.ErrPolicyNotFound
@@ -64,4 +98,51 @@ func (pl *PolicyList) GetPolicy(name string) (PolicyData, error){
 	}
 
 	return pl.Policies[name], nil
+}
+
+func (pl *PolicyList) IterPolicies() <- chan PolicyData{
+	ch := make(chan PolicyData)
+	go func(){
+		for _, policy := range pl.Policies {
+			ch <- policy
+		}
+		close(ch)
+	}();
+	return ch
+}
+
+func (pd *PolicyData) IsAllowed(action string, resource string) bool{
+	return pd.Action == action && pd.Resource == resource
+}
+
+func (pd *PolicyData) GetPolicyName() string{
+	return pd.Name
+}
+
+func (pd *PolicyData) GetPolicyDescription() string{
+	return pd.Description
+}
+
+func (pd *PolicyData) GetPolicyAction() string{
+	return pd.Action
+}
+
+func (pd *PolicyData) GetPolicyResource() string{
+	return pd.Resource
+}
+
+func (pd *PolicyData) SetPolicyName(name string){
+	pd.Name = name
+}
+
+func (pd *PolicyData) SetPolicyDescription(description string){
+	pd.Description = description
+}
+
+func (pd *PolicyData) SetPolicyAction(action string){
+	pd.Action = action
+}
+
+func (pd *PolicyData) SetPolicyResource(resource string){
+	pd.Resource = resource
 }
