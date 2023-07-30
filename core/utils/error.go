2511985e2e07cc566ae2adeb9ca935330b510319
Shayan <shayan1997g@gmail.com>
2023-07-31 02:09:04 +0330

    ADDED: updated error handler, needs to be rewritten

--- core/utils/error.go
+++ core/utils/error.go
@@ -1,12 +1,42 @@
 package utils
 
-import "strings"
+import (
+	"strings"
 
-type CustomError string
+)
+
+
+type Error interface{
+	Error() string
+	String() string
+	Errorf(format string, a ...interface{String() string}) string
+}
+
+type CustomError struct {
+	msg string
+}
 
 func (e CustomError) Error() string {
 	var sb strings.Builder
 	sb.WriteString("Error: ")
-	sb.WriteString(string(e))
+	sb.WriteString(e.msg)
 	return sb.String()
 }
+
+func (e CustomError) String() string {
+	return e.Error()
+}
+
+func (e CustomError) Errorf(format string, a ...interface{String() string}) string {
+	var sb strings.Builder
+	sb.WriteString("Error: ")
+	sb.WriteString(e.msg)
+	sb.WriteString(" ")
+	sb.WriteString(format)
+	sb.WriteString(" ")
+	for _, arg := range a {
+		sb.WriteString(arg.String())
+	}
+	return sb.String()
+}
+// TODO: rework error handling entirely
